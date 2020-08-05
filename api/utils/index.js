b659dc8ed368fe90ba6ca749f1ce794b6b1e23a6
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-08-05 20:49:40 +0100

    save events works again

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,26 +1,23 @@
-const jwt = require('jsonwebtoken')
-const UserModel = require('../models/users.model')
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/users.model");
 
 // Authenticate Middleware
-function authUser (req, res, next) {
+function authUser(req, res, next) {
   if (!req.headers.token) {
-    res.status(403).json({ error: 'No Token found' })
+    res.status(403).json({ error: "No Token found" });
   } else {
     try {
-      const decodedToken = jwt.verify(req.headers.token, process.env.SECRET)
-
-      UserModel
-        .findOne({ email: decodedToken.email })
-        .then(user => {
-          res.locals.user = user
-          next()
-        })
+      const decodedToken = jwt.verify(req.headers.token, process.env.SECRET);
+      UserModel.findOne({ email: decodedToken.email }).then((user) => {
+        res.locals.user = user;
+        next();
+      });
     } catch (error) {
-      res.status(403).json({ error: `Token not valid + ${error}` })
+      res.status(403).json({ error: `Token not valid + ${error}` });
     }
   }
 }
 
 module.exports = {
-  authUser
-}
+  authUser,
+};
