ae31998b99d1dbfe36c05c81b1e63d374def9cfb
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-08-12 12:44:28 +0100

    edited tags creation, added auth requirement

--- api/controllers/tags.controller.js
+++ api/controllers/tags.controller.js
@@ -1,4 +1,6 @@
 const TagModel = require("../models/tags.model");
+const UserModel = require("../models/users.model");
+const EventModel = require("../models/events.model");
 
 function getAllTags(req, res) {
   TagModel.find(req.query)
@@ -14,11 +16,21 @@ function createTag(req, res) {
     .then((tag) => {
       if (tag.length === 0) {
         TagModel.create(tagInput)
-          .then((response) => {
+          .then((tagCreate) => {
+            const tagId = tagCreate._id;
+            EventModel.findById(tagInput.eventId).then((event) => {
+              event.tags.push(tagId);
+              event.save();
+            });
             res.json(response);
           })
           .catch((err) => res.json(err));
       } else {
+        EventModel.findById(tagInput.eventId).then((event) => {
+          event.tags.push(tag[0]._id);
+          console.log(event.tags);
+          event.save();
+        });
         tag[0].useCount += 1;
         tag[0].save();
         res.json(tag);
@@ -31,3 +43,38 @@ module.exports = {
   getAllTags,
   createTag,
 };
+
+// function createTag(req, res) {
+//   const tagInput = req.body;
+//   const userId = res.locals.user._id;
+//   TagModel.find({ name: tagInput.name })
+//     .then((tag) => {
+//       if (tag.length === 0) {
+//         TagModel.create(tagInput)
+//           .then((tagCreate) => {
+//             const tagId = tagCreate._id;
+//             UserModel.findById(userId)
+//               .populate("myEvents")
+//               .then((user) => {
+//                 user.myEvents.forEach((event) => {
+//                   if (
+//                     JSON.stringify(event._id) ===
+//                     JSON.stringify(tagInput.eventId)
+//                   ) {
+//                     event.tags.push(tagId);
+//                     console.log(event.tags);
+//                   }
+//                 });
+//                 user.save();
+//               });
+//             res.json(response);
+//           })
+//           .catch((err) => res.json(err));
+//       } else {
+//         tag[0].useCount += 1;
+//         tag[0].save();
+//         res.json(tag);
+//       }
+//     })
+//     .catch((err) => res.json(err));
+// }
--- api/routes/tags.router.js
+++ api/routes/tags.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const { authUser } = require("../utils");
 
 const { getAllTags, createTag } = require("../controllers/tags.controller");
 
-router.get("/", getAllTags).post("/createTag", createTag);
+router.get("/", getAllTags).post("/createTag", authUser, createTag);
 
 module.exports = router;
